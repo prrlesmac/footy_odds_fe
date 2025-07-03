a8e5a9d60f1702e94d0f243900db88aba3301641
prrlesmac <prrlesmac@gmail.com>
2025-07-03 08:58:41 -0600

    add UCL

--- app.py
+++ app.py
@@ -7,19 +7,37 @@ app = Flask(__name__)
 
 def get_football_data(league):
     engine = db.get_postgres_engine()
-    query = f"""
-    SELECT 
-        team,
-        elo,
-        champion,
-        top_4,
-        {'relegation_playoff,' if league in ['GER','FRA'] else ''}
-        relegation_direct
-    FROM public.sim_standings_dom s 
-    LEFT JOIN public.current_elos c 
-    ON s.team = c.club
-    WHERE s.league='{league}'
-    """
+    if league in ['UCL', 'UEL', 'UECL']:
+        query = f"""
+        SELECT 
+            team,
+            elo,
+            po_r32,
+            po_r16,
+            po_r8,  
+            po_r4,
+            po_r2,
+            po_champion
+        FROM public.sim_standings_con s 
+        LEFT JOIN public.current_elos c 
+        ON s.team = c.club
+        WHERE s.league='{league}'
+        """
+    
+    else:
+        query = f"""
+        SELECT 
+            team,
+            elo,
+            champion,
+            top_4,
+            {'relegation_playoff,' if league in ['GER','FRA'] else ''}
+            relegation_direct
+        FROM public.sim_standings_dom s 
+        LEFT JOIN public.current_elos c 
+        ON s.team = c.club
+        WHERE s.league='{league}'
+        """
     standings = pd.read_sql(query, engine)
     return standings
 
@@ -28,7 +46,6 @@ def index():
     leagues = list(config.league_mapping.keys())
     selected_league = request.args.get('league', leagues[0])
     df = get_football_data(config.league_mapping[selected_league])
-    data=df.to_dict(orient='records')
     return render_template(
         'table.html',
         data=df.to_dict(orient='records'),
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 league_mapping = {
+    'UEFA Champions League': 'UCL',
+  #  'UEFA Europa League': 'UEL',
+  #  'UEFA Conference League': 'UECL',
     'Premier League (England)': 'ENG',
     'La Liga (Spain)': 'ESP',
     'Serie A (Italy)': 'ITA',
