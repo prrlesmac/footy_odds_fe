b69580c45e4b3e7e13add9d17602d95801d6bcbd
prrlesmac <prrlesmac@gmail.com>
2025-07-04 09:29:37 -0600

    add uel and conf to fe

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 league_mapping = {
     'UEFA Champions League': 'UCL',
-  #  'UEFA Europa League': 'UEL',
-  #  'UEFA Conference League': 'UECL',
+    'UEFA Europa League': 'UEL',
+    'UEFA Conference League': 'UECL',
     'Premier League (England)': 'ENG',
     'La Liga (Spain)': 'ESP',
     'Serie A (Italy)': 'ITA',
